da3a357299b9e5de19d4a168818324b1a8bd8de9
Faye <fayepeteros@gmail.com>
2025-02-28 19:34:17 +0800

    Edited logo spacing

---
+++ script.js
@@ -0,0 +1,18 @@
+// Dropdown Button
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.shop-btn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-list");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
