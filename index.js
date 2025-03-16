5ee51c66b1ff4e5885d5c5f8d2255527c835da2e
Faye <fayepeteros@gmail.com>
2025-03-17 02:00:53 +0800

    Attempt at cart functionality

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Js for the dropdown menu
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -15,22 +16,66 @@ window.onclick = function(event) {
     }
   }
 
-  document.addEventListener("DOMContentLoaded", () => {
-    const cartIcon = document.querySelector(".shopping-cart-container");
-    const closeCartBtn = document.querySelector(".close-cart-btn");
-    const body = document.body;
 
-    // Ensure cart is hidden when page loads
-    body.classList.remove("show-cart");
+  //JS for the cart
+  document.addEventListener("DOMContentLoaded", () => {
+    let cartIcon = document.querySelector(".shopping-cart-container");
+    let closeCartBtn = document.querySelector(".close-cart-btn");
+    let body = document.body;
+    let listProductsHTML = document.querySelector(".cart-list");
+
+    let listProducts = [];
 
     cartIcon.addEventListener("click", (event) => {
         event.preventDefault();
         body.classList.toggle("show-cart");
     });
 
+
     closeCartBtn.addEventListener("click", () => {
-        body.classList.remove("show-cart");
+          event.preventDefault();
+          body.classList.toggle("show-cart");
+
     });
+
+    const addDatatoHTML = () => {
+      listProductsHTML.innerHTML = ' ';
+      if(listProducts.length > 0) {
+          listProducts.forEach (product => {
+            let newProduct = document.createElement('div');
+            newProduct.classList.add('cart-item');
+            newProduct.innerHTML = `
+                <div class="cart-item-img">
+                    <img src="${product.image}  " alt="">
+                </div>
+                <div class="cart-item-name">
+                    Brow Pen Bean Yo mama
+                </div>
+                <div class="total-price">
+                    $300
+                </div>
+                <div class="quantity">
+                    <span class="minus">&lt;</span>
+                    <span>1</span>
+                    <span class="plus">&gt;</span>
+                </div>
+            `;
+            listProductsHTML.appendChild(newProduct);
+          })
+      }
+    }
+
+    const initApp = () => {
+      //Get data from json file
+      fetch('products.json')
+      .then(response => response.json())
+      .then(data => {
+          listProducts = data;
+          addDatatoHTML();
+      })
+    }
+
+    initApp();
 });
 
 
