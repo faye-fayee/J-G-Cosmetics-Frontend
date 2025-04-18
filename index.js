72e44a0104241500e9bea35d32655363ed8c733d
Faye <fayepeteros@gmail.com>
2025-04-18 18:01:40 +0800

    Add Dynamic Account Details Page

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const accountLink = document.getElementById("account-link");
+
+  if (localStorage.getItem("userId")) {
+      accountLink.setAttribute("href", "acc-details.html");
+  } else {
+      accountLink.setAttribute("href", "log-in.html");
+  }
+});
+
 //Js for the dropdown menu
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
